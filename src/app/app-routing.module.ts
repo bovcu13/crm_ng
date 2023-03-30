57b3df6a6a7f0af5f3c2af0c1617017bfe04063d
zowie <C109118211@nkust.edu.tw>
2023-03-30 14:49:36 +0800

    建立各component並連上panel

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { DashboardComponent } from './views/features/dashboard/dashboard.component';
+import { LeadComponent } from './views/features/lead/lead.component';
+import { AccountComponent } from './views/features/account/account.component';
+import { ContactComponent } from './views/features/contact/contact.component';
+import { OpportunityComponent } from './views/features/opportunity/opportunity.component';
+import { QuoteComponent } from './views/features/quote/quote.component';
+import { CampaignComponent } from './views/features/campaign/campaign.component';
+import { ForecastingComponent } from './views/features/forecasting/forecasting.component';
+import { ReportComponent } from './views/features/report/report.component';
+import { EventComponent } from './views/features/event/event.component';
 const routes: Routes = [
-  {path: '', component: DashboardComponent}
+  {path: '', component: DashboardComponent},
+  {path: 'Lead', component: LeadComponent},
+  {path: 'Account', component: AccountComponent},
+  {path: 'Contact', component: ContactComponent},
+  {path: 'Opportunity', component: OpportunityComponent},
+  {path: 'Quote', component: QuoteComponent},
+  {path: 'Campaign', component: CampaignComponent},
+  {path: 'Forecasting', component: ForecastingComponent},
+  {path: 'Report', component: ReportComponent},
+  {path: 'Event', component: EventComponent},
 ];
 
 @NgModule({
