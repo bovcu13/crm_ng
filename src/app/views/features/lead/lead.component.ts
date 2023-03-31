98d2b74ea2cbef3bb4d59f5d8d9e909008c47eb9
woni <jessica901215@gmail.com>
2023-03-31 17:03:03 +0800

    feat: lead table

--- src/app/views/features/lead/lead.component.ts
+++ src/app/views/features/lead/lead.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
-
+interface lead {
+  name: string;
+  position: string;
+  company: string;
+  phone_number: string;
+  cell_phone: string;
+  email: string;
+}
 @Component({
   selector: 'app-lead',
   templateUrl: './lead.component.html',
   styleUrls: ['./lead.component.scss']
 })
 export class LeadComponent {
+  lead: lead[] = [];
+  ngOnInit(): void {
+    this.lead=[
 
+    ]
+  }
 }
