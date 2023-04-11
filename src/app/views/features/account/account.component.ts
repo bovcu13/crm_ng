b99383212a58b8adefb4c68de1a0dc304ff424ea
zowie <C109118211@nkust.edu.tw>
2023-04-12 02:58:42 +0800

    style: 帳戶業務負責人&創建者&更新者 disable

--- src/app/views/features/account/account.component.ts
+++ src/app/views/features/account/account.component.ts
@@ -85,6 +85,11 @@ export class AccountComponent {
   dialogHeader!: string;
   showDialog(type: string, account?: any): void {
     this.edit = true;
+    this.account_form.controls['account_owner'].disable();
+    this.account_form.controls['created_by'].disable();
+    this.account_form.controls['updated_by'].disable();
+    this.account_form.controls['created_at'].disable();
+    this.account_form.controls['updated_at'].disable();
     if (type === 'add') {
       this.dialogHeader = '新增';
       this.account_form.reset();
