61907efca92027e1f1fb787a7007b87476263540
zowie <C109118211@nkust.edu.tw>
2023-04-16 04:04:29 +0800

    feat: event color

--- src/app/views/features/event/event.component.ts
+++ src/app/views/features/event/event.component.ts
@@ -1,15 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { Calendar } from 'primeng/calendar';
-
-interface employee_name {
-  name: string;
-}
-
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Calendar} from 'primeng/calendar';
 @Component({
   selector: 'app-event',
   templateUrl: './event.component.html',
@@ -18,7 +13,30 @@ interface employee_name {
 export class EventComponent {
   @ViewChild('startDate') startDate!: Calendar;
   @ViewChild('endDate') endDate!: Calendar;
-  employee_name: employee_name[] = [];
+  //指派對象dropdown值
+  assigned_to: any[] = [
+      {name: '123'},
+      {name: '李小名'},
+      {name: '李小名'},
+  ];
+  //類型dropdown值
+  type: any[] = [
+    {
+      "name": "Email",
+      "code": "email"
+    },
+    {
+      "name": "Meeting",
+      "code": "meeting."
+    },
+    {
+      "name": "Call",
+      "code": "call"
+    },
+    {
+      "name": "Other",
+      "code": "other"
+    }]
   options: any;
   //建立formgroup表單
   event_form: FormGroup;
@@ -31,12 +49,14 @@ export class EventComponent {
       all_date: ['', [Validators.required]],
       start_date: ['', [Validators.required]],
       end_date: ['', [Validators.required]],
+      type: [''],
     });
     //驗證日期是否有效
     if (this.event_form.controls['start_date'].value > this.event_form.controls['end_date'].value) {
-      this.event_form.controls['end_date'].setErrors({ 'incorrect': true });
+      this.event_form.controls['end_date'].setErrors({'incorrect': true});
     }
   }
+
   ngOnInit() {
     // 判斷全天日期是否有被選取
     const alldayControl = this.event_form.get('allday');
@@ -59,11 +79,7 @@ export class EventComponent {
         }
       });
     }
-    this.employee_name = [
-      { name: '123' },
-      { name: '李小名' },
-      { name: '李小名' },
-    ]
+    //日曆控制選項
     this.options = {
       plugins: [
         interactionPlugin,
@@ -71,7 +87,7 @@ export class EventComponent {
         timeGridPlugin,
         listPlugin,
       ],
-      initialView: 'timeGridWeek',//日曆面板是否顯示以甚麼為單位的日期
+      initialView: 'dayGridMonth',//日曆面板是否顯示以甚麼為單位的日期
       selectable: false, //是否可以選取多個日期
       selectOverlap: true, //日期是否可以被重複選中多次
       weekends: true, //日曆面板中是否顯示周末,一開始為不顯示
@@ -86,15 +102,57 @@ export class EventComponent {
       dayMaxEvents: true,
       handleWindowResize: true, //隨瀏覽器窗口大小變化
       events: [
-        { title: 'event 1', start: '2023-04-01T06:30:00', end: '2023-04-01T08:30:00', allDay: false },
-        { title: 'event 2', date: '2023-04-03', allDay: true },
-        { title: 'event 3', date: '2023-04-09', allDay: true },
-        { title: 'event 4', start: '2023-04-04T14:30:00', end: '2023-04-04T16:30:00', allDay: false },
-        { title: 'event 5', start: '2023-04-25T19:00:00', end: '2023-04-26T21:30:00', allDay: false },
-        { title: 'event 6', start: '2023-04-06T13:30:00', end: '2023-04-07T15:10:00', allDay: false },
-        { title: 'event 7', start: '2023-04-17T09:30:00', end: '2023-04-18T10:30:00', allDay: false },
-        { title: 'event 8', start: '2023-04-08T16:30:00', end: '2023-04-08T17:00:00', allDay: false },
-        { title: 'event 9', start: '2023-04-19T10:30:00', end: '2023-04-19T12:00:00', allDay: false },
+        {
+          title: 'event 1',
+          start: '2023-04-01T06:30:00',
+          end: '2023-04-01T08:30:00',
+          allDay: false,
+          backgroundColor: 'red'
+        },
+        {title: 'event 2', date: '2023-04-03', allDay: true, backgroundColor: 'orange'},
+        {title: 'event 3', date: '2023-04-09', allDay: true, backgroundColor: 'yellow'},
+        {
+          title: 'event 4',
+          start: '2023-04-04T14:30:00',
+          end: '2023-04-04T16:30:00',
+          allDay: false,
+          backgroundColor: 'green'
+        },
+        {
+          title: 'event 5',
+          start: '2023-04-25T19:00:00',
+          end: '2023-04-26T21:30:00',
+          allDay: false,
+          backgroundColor: 'deepskyblue'
+        },
+        {
+          title: 'event 6',
+          start: '2023-04-06T13:30:00',
+          end: '2023-04-07T15:10:00',
+          allDay: false,
+          backgroundColor: 'pink'
+        },
+        {
+          title: 'event 7',
+          start: '2023-04-17T09:30:00',
+          end: '2023-04-18T10:30:00',
+          allDay: false,
+          backgroundColor: 'purple'
+        },
+        {
+          title: 'event 8',
+          start: '2023-04-08T16:30:00',
+          end: '2023-04-08T17:00:00',
+          allDay: false,
+          backgroundColor: 'deepblue'
+        },
+        {
+          title: 'event 9',
+          start: '2023-04-19T10:30:00',
+          end: '2023-04-19T12:00:00',
+          allDay: false,
+          backgroundColor: 'gray'
+        },
       ],
       //點選日期開啟新增事件
       dateClick: () => {
@@ -109,11 +167,12 @@ export class EventComponent {
       },
     };
   }
+
   //時間調整
   localToUtc(date: Date): Date {
     return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours() - 8, date.getMinutes(), date.getSeconds()));
   }
-
+  //新增編輯日曆事件dialog控制項
   visible: boolean = false;
   dialogHeader!: string;
   showDialog(type: string, event?: any) {
@@ -129,6 +188,10 @@ export class EventComponent {
     } else {
       this.event_form.reset();
     }
+  }
+  typeValue(event: any): void {
+    const typeValue = this.type.find((s: { code: any; }) => s.code === event.value.code);
+    console.log(event.value.code , typeValue.name);
   }
 }
 
