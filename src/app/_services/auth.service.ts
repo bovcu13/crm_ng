677890a35c0351c4e168f7a8ca51a16bd1db2533
woni <jessica901215@gmail.com>
2023-04-20 16:48:00 +0800

    feat: 取得token & 將token存到session storage

--- src/app/_services/auth.service.ts
+++ src/app/_services/auth.service.ts
@@ -1,28 +1,27 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
-const AUTH_API = 'https://api.t.d2din.com/crm/';
+const AUTH_API = 'https://api.t.d2din.com';
 
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
-  login(username: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'login', {
-      username,
-      password
-    }, httpOptions);
+  login(body: any): Observable<any> {
+    const url = `${AUTH_API}/crm/v1.0/login`;
+    return this.http.post(url, body, httpOptions);
   }
 
   register(username: string, email: string, password: string): Observable<any> {
-    return this.http.post(AUTH_API + 'signin', {
+    return this.http.post(AUTH_API + '/crm/v1.0/signin', {
       username,
       email,
       password
--- src/app/views/components/login/login.component.ts
+++ src/app/views/components/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService} from "../../../_services/auth.service";
-import { TokenStorageService} from "../../../_services/token-storage.service";
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from "../../../_services/auth.service";
+import {TokenStorageService} from "../../../_services/token-storage.service";
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -8,16 +9,20 @@ import { TokenStorageService} from "../../../_services/token-storage.service";
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  form: any = {
-    username: null,
-    password: null
-  };
+  login_form: FormGroup;
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
   roles: string[] = [];
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private fb: FormBuilder) {
+    this.login_form = this.fb.group({
+      //必填
+      user_name: ['admin', [Validators.required]],
+      password: ['12345', [Validators.required]],
+      company_id: ['1a8e81ed-8ec7-4cba-831a-77ea4079b3fd', [Validators.required]],
+    });
+  }
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
@@ -27,12 +32,15 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
-    const { username, password } = this.form;
-
-    this.authService.login(username, password).subscribe(
+    let body = {
+      "user_name": this.login_form.controls['user_name'].value,
+      "company_id": this.login_form.controls['company_id'].value,
+      "password": this.login_form.controls['password'].value
+    }
+    this.authService.login(body).subscribe(
       data => {
-        this.tokenStorage.saveToken(data.accessToken);
-        this.tokenStorage.saveRefreshToken(data.refreshToken);
+        this.tokenStorage.saveToken(data.body.access_token);
+        this.tokenStorage.saveRefreshToken(data.body.refresh_token);
         this.tokenStorage.saveUser(data);
 
         this.isLoginFailed = false;
