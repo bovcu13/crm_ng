c41ce370d2882ade85db3083122590907f593fc4
woni <jessica901215@gmail.com>
2023-04-21 16:29:54 +0800

    feat: 帳密驗證導入頁面

--- src/app/views/components/login/login.component.ts
+++ src/app/views/components/login/login.component.ts
@@ -1,7 +1,6 @@
-import {Component, OnInit} from '@angular/core';
-import {AuthService} from "../../../_services/auth.service";
-import {TokenStorageService} from "../../../_services/token-storage.service";
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { AuthService} from "../../../_services/auth.service";
+import { TokenStorageService} from "../../../_services/token-storage.service";
 
 @Component({
   selector: 'app-login',
@@ -9,20 +8,16 @@ import {FormBuilder, FormGroup, Validators} from '@angular/forms';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  login_form: FormGroup;
+  form: any = {
+    username: null,
+    password: null
+  };
   isLoggedIn = false;
   isLoginFailed = false;
   errorMessage = '';
   roles: string[] = [];
 
-  constructor(private authService: AuthService, private tokenStorage: TokenStorageService, private fb: FormBuilder) {
-    this.login_form = this.fb.group({
-      //必填
-      user_name: ['admin', [Validators.required]],
-      password: ['12345', [Validators.required]],
-      company_id: ['1a8e81ed-8ec7-4cba-831a-77ea4079b3fd', [Validators.required]],
-    });
-  }
+  constructor(private authService: AuthService, private tokenStorage: TokenStorageService) { }
 
   ngOnInit(): void {
     if (this.tokenStorage.getToken()) {
@@ -32,15 +27,12 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(): void {
-    let body = {
-      "user_name": this.login_form.controls['user_name'].value,
-      "company_id": this.login_form.controls['company_id'].value,
-      "password": this.login_form.controls['password'].value
-    }
-    this.authService.login(body).subscribe(
+    const { username, password } = this.form;
+
+    this.authService.login(username, password).subscribe(
       data => {
-        this.tokenStorage.saveToken(data.body.access_token);
-        this.tokenStorage.saveRefreshToken(data.body.refresh_token);
+        this.tokenStorage.saveToken(data.accessToken);
+        this.tokenStorage.saveRefreshToken(data.refreshToken);
         this.tokenStorage.saveUser(data);
 
         this.isLoginFailed = false;
