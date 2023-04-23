83b3e83d218616babb5b6111208a7e010dff64a5
zowie <C109118211@nkust.edu.tw>
2023-04-23 20:57:29 +0800

    feat: 行銷活動增加表格查詢& style: 訂單調整報價inputNumber-scss

--- src/app/views/features/campaign/campaign.component.ts
+++ src/app/views/features/campaign/campaign.component.ts
@@ -1,18 +1,19 @@
 import { Component, ViewChild } from '@angular/core';
-import { MenuItem } from 'primeng/api';
 import { FormBuilder, FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
 import { Calendar } from 'primeng/calendar';
+import { Table } from 'primeng/table';
 @Component({
   selector: 'app-campaign',
   templateUrl: './campaign.component.html',
   styleUrls: ['./campaign.component.scss']
 })
+
 export class CampaignComponent {
   @ViewChild('startDate') startDate: Calendar | undefined;
-  @ViewChild('endDate') endDate: Calendar| undefined;
+  @ViewChild('endDate') endDate: Calendar | undefined;
   //table內容
-  campaign: any = { name: "123", patrilineal_name: "", type: "Event", status: "Aborted", start_date: "2023-04-07", end_date: "2023-04-09", responses: "40", owner: "王大明" };
-  campaign_table: any[] = [
+  filteredCampaigns: any[] = [];
+  campaign: any[] = [
     {
       name: "1+1",
       enable: true,
@@ -21,7 +22,6 @@ export class CampaignComponent {
       status: "策劃中",
       start_date: "2023-04-07",
       end_date: "2023-04-09",
-      responses: "40",
       owner: "王大明",
       created_at: "2023-04-15",
       created_by: "林",
@@ -35,14 +35,41 @@ export class CampaignComponent {
       status: "已完成",
       start_date: "2023-04-14",
       end_date: "2023-04-19",
-      responses: "30",
       owner: "王大明",
       created_at: "2023-04-15",
       created_by: "林",
       updated_by: "林",
     },
   ];
-
+  filterText: any = '';
+  filterCampaigns() {
+    if (this.filterText) {
+      this.filteredCampaigns = this.campaign.filter((campaign) => {
+        return campaign.name.toLowerCase().includes(this.filterText.toLowerCase());
+      });
+    } else {
+      this.filteredCampaigns = this.campaign;
+    }
+    if (this.filterText === '') {
+      this.filteredCampaigns = this.campaign;
+    } else {
+      this.filteredCampaigns = this.campaign.filter(campaign => {
+        return (
+          campaign.name.toLowerCase().includes(this.filterText) ||
+          campaign.patrilineal_name.toLowerCase().includes(this.filterText) ||
+          campaign.type.toLowerCase().includes(this.filterText) ||
+          campaign.status.toLowerCase().includes(this.filterText) ||
+          campaign.start_date.toLowerCase().includes(this.filterText) ||
+          campaign.end_date.toLowerCase().includes(this.filterText) ||
+          campaign.owner.toLowerCase().includes(this.filterText)
+        );
+      });
+    }
+    console.log(this.filteredCampaigns)
+  }
+  ngOnInit() {
+    this.filteredCampaigns = this.campaign;
+  }
   //p-dropdown狀態
   status = [
     {
@@ -100,14 +127,13 @@ export class CampaignComponent {
 
   //建立formgroup表單
   campaign_form: FormGroup;
-  currentCampaign!: any;
   start_date!: Date;
   end_date!: Date;
 
   constructor(private fb: FormBuilder) {
     this.campaign_form = this.fb.group({
       name: ['', [Validators.required]],
-      owner: [''],
+      owner: new FormControl({ value: '', disabled: true }),
       enable: [false],
       status: [''],
       patrilineal_name: [''],
@@ -117,26 +143,39 @@ export class CampaignComponent {
       description: [''],
       sent: [''],
       budget_cost: [''],
-      responses: [''],
+      expected_responses: [''],
       actual_cost: [''],
       expected_income: [''],
-    });
+      created_at: new FormControl({ value: '', disabled: true }),
+      updated_at: new FormControl({ value: '', disabled: true }),
+      created_by: new FormControl({ value: '', disabled: true }),
+      updated_by: new FormControl({ value: '', disabled: true }),
+    }, { formGroupName: 'campaign' });
     //驗證日期是否有效
     if (this.campaign_form.controls['start_date'].value > this.campaign_form.controls['end_date'].value) {
       this.campaign_form.controls['end_date'].setErrors({ 'incorrect': true });
     }
   }
-  ngOnInit() {
+
+  //偵測status變量
+  onStatusChange(event: any) {
+    console.log("狀態選擇status: " + event.value.code + event.value.name);
   }
+
   //dialog方法
   visible: boolean = false;
   dialogHeader!: string;
+  selectedStatus!: any;
+  showedit = true;//判斷是否dialog為新增與編輯
   showDialog(type: string, campaign?: any): void {
     this.visible = true;
-    console.log('Invalid type: ' + type);
+    console.log('點擊: ' + type);
+    this.campaign_form.reset()
+    this.campaign_form.patchValue({ status: this.status[0].name});
+    console.log(this.status)
     if (type === 'add') {
       this.dialogHeader = '新增行銷活動';
-      this.campaign_form.reset();
+      this.showedit = false;
     } else if (type === 'edit') {
       console.log("campaign: " + JSON.stringify(campaign))
       this.dialogHeader = '編輯行銷活動';
@@ -145,6 +184,9 @@ export class CampaignComponent {
         start_date: new Date(campaign.start_date),
         end_date: new Date(campaign.end_date)
       });
+      this.showedit = true;
+      // 綁定已經選擇的狀態
+      this.selectedStatus = this.status.find(s => s.name === campaign.status);
     }
   }
 }
