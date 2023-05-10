173bb93e398ba76cecad9ba5738b2d6da845766b
Hsxxnil <lhsuan1225@gmail.com>
2023-05-11 00:01:41 +0800

    fix: 行事曆標頭位置

--- src/app/views/features/event/event.component.ts
+++ src/app/views/features/event/event.component.ts
@@ -1,10 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 import listPlugin from '@fullcalendar/list';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Calendar } from 'primeng/calendar';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {Calendar} from 'primeng/calendar';
 
 @Component({
   selector: 'app-event',
@@ -17,21 +17,21 @@ export class EventComponent {
   @ViewChild('endDate') endDate!: Calendar;
   //主辦人dropdown值
   main: any[] = [
-    { "name": "王大天", },
-    { "name": '李小名' },
-    { "name": '123456' },
-    { "name": 'test1' },
-    { "name": 'test2' },
-    { "name": 'test3' },
+    {"name": "王大天",},
+    {"name": '李小名'},
+    {"name": '123456'},
+    {"name": 'test1'},
+    {"name": 'test2'},
+    {"name": 'test3'},
   ];
   //參加成員dropdown值
   member: any[] = [
-    { "name": '1' },
-    { "name": '2' },
-    { "name": '3' },
-    { "name": '4' },
-    { "name": '5' },
-    { "name": '6' },
+    {"name": '1'},
+    {"name": '2'},
+    {"name": '3'},
+    {"name": '4'},
+    {"name": '5'},
+    {"name": '6'},
   ];
   //類型dropdown值
   type: any[] = [
@@ -72,6 +72,7 @@ export class EventComponent {
   options: any;
   //建立formgroup表單
   event_form: FormGroup;
+
   constructor(private fb: FormBuilder) {
     this.event_form = this.fb.group({
       subject: ['', [Validators.required]],
@@ -88,9 +89,10 @@ export class EventComponent {
     });
     //驗證日期是否有效
     if (this.event_form.controls['start_date'].value > this.event_form.controls['end_date'].value) {
-      this.event_form.controls['end_date'].setErrors({ 'incorrect': true });
+      this.event_form.controls['end_date'].setErrors({'incorrect': true});
     }
   }
+
   ngOnInit() {
     // 判斷全天日期是否有被選取
     // const alldayControl = this.event_form.get('allday');
@@ -127,8 +129,8 @@ export class EventComponent {
       weekends: true, //日曆面板中是否顯示周末,一開始為不顯示
       //initialDate: 2023-03-31 //畫面一開始會顯示的日期
       headerToolbar: {// 日曆表頭部分
-        left: 'prev next today',
-        center: 'title',
+        left: 'today',
+        center: 'prev,title,next',
         right: 'dayGridMonth,timeGridWeek,timeGridDay',
       },
       editable: false,// 是否可以進行拖拽、修改
@@ -138,8 +140,8 @@ export class EventComponent {
       events: [
         {
           title: '緊急事件',
-          main: [{ "name": "test1" }, { 'name': "test2" }],
-          member: [{ "name": "1" }, { 'name': "3" }],
+          main: [{"name": "test1"}, {'name': "test2"}],
+          member: [{"name": "1"}, {'name': "3"}],
           start: '2023-05-01T06:30:00',
           end: '2023-05-01T08:30:00',
           allDay: false,
@@ -147,19 +149,19 @@ export class EventComponent {
           type: '電話',
         },
         {
-          title: '客戶拜訪', main: [{ "name": "123456" }, { 'name': "王大天" }],
-          member: [{ "name": "6" }, { 'name': "4" }], date: '2023-05-03', allDay: true, backgroundColor: 'orange',
+          title: '客戶拜訪', main: [{"name": "123456"}, {'name': "王大天"}],
+          member: [{"name": "6"}, {'name': "4"}], date: '2023-05-03', allDay: true, backgroundColor: 'orange',
           type: '客戶拜訪', textColor: 'black',
         },
         {
-          title: '電話', main: [{ "name": "李小名" }, { 'name': "王大天" }],
-          member: [{ "name": "2" }], date: '2023-05-09', allDay: true, backgroundColor: 'burlywood', type: '電話',
+          title: '電話', main: [{"name": "李小名"}, {'name': "王大天"}],
+          member: [{"name": "2"}], date: '2023-05-09', allDay: true, backgroundColor: 'burlywood', type: '電話',
           textColor: 'black'
         },
         {
           title: '會議',
-          main: [{ "name": "李小名" }, { 'name': "test" }],
-          member: [{ "name": "2" }, { 'name': "4" }],
+          main: [{"name": "李小名"}, {'name': "test"}],
+          member: [{"name": "2"}, {'name': "4"}],
           start: '2023-05-04T14:30:00',
           end: '2023-05-04T16:30:00',
           allDay: false,
@@ -169,8 +171,8 @@ export class EventComponent {
         },
         {
           title: '研討會',
-          main: [{ "name": "123456" }, { 'name': "test3" }],
-          member: [{ "name": "5" }, { 'name': "4" }],
+          main: [{"name": "123456"}, {'name': "test3"}],
+          member: [{"name": "5"}, {'name': "4"}],
           start: '2023-05-25T19:00:00',
           end: '2023-05-26T21:30:00',
           allDay: false,
@@ -178,8 +180,8 @@ export class EventComponent {
         },
         {
           title: '行銷',
-          main: [{ 'name': "李小名" }, { 'name': "王大天" }],
-          member: [{ "name": "6" }, { 'name': "4" }],
+          main: [{'name': "李小名"}, {'name': "王大天"}],
+          member: [{"name": "6"}, {'name': "4"}],
           start: '2023-05-06T13:30:00',
           end: '2023-05-07T15:10:00',
           allDay: false,
@@ -187,8 +189,8 @@ export class EventComponent {
         },
         {
           title: '年度拜訪',
-          main: [{ 'name': "test1" }, { 'name': "王大天" }],
-          member: [{ "name": "6" }],
+          main: [{'name': "test1"}, {'name': "王大天"}],
+          member: [{"name": "6"}],
           start: '2023-05-17T09:30:00',
           end: '2023-05-18T10:30:00',
           allDay: false,
@@ -196,8 +198,8 @@ export class EventComponent {
         },
         {
           title: '會議',
-          main: [{ "name": "123456" }, { 'name': "test1" }],
-          member: [{ "name": "1" }, { 'name': "3" }],
+          main: [{"name": "123456"}, {'name': "test1"}],
+          member: [{"name": "1"}, {'name': "3"}],
           start: '2023-05-08T16:30:00',
           end: '2023-05-08T17:00:00',
           allDay: false,
@@ -205,8 +207,8 @@ export class EventComponent {
         },
         {
           title: '客戶拜訪',
-          main: [{ "name": "test" }, { 'name': "test1" }],
-          member: [{ "name": "6" }, { 'name': "3" }],
+          main: [{"name": "test"}, {'name': "test1"}],
+          member: [{"name": "6"}, {'name': "3"}],
           start: '2023-05-19T10:30:00',
           end: '2023-05-19T12:00:00',
           allDay: false,
@@ -226,43 +228,43 @@ export class EventComponent {
           + info.event.allDay + "/" + " type: " + info.event.extendedProps.type);
       },
     };
-}
+  }
 
 //新增編輯日曆事件dialog控制項
-visible: boolean = false;
-dialogHeader!: string;
-showedit = true;//判斷是否dialog為新增與編輯
+  visible: boolean = false;
+  dialogHeader!: string;
+  showedit = true;//判斷是否dialog為新增與編輯
   showDialog(type: string, event ?: any) {
-  this.dialogHeader = type === 'edit' ? '編輯日曆事件' : '新增日曆事件';
-  this.visible = true;
-  if (event && event.main) {
-    this.event_form.setValue({
-      main: event.extendedProps.main.split(',')?.map((name: string) => ({ name })),
-      member: event.extendedProps.member.split(',')?.map((name: string) => ({ name })),
-    });
+    this.dialogHeader = type === 'edit' ? '編輯日曆事件' : '新增日曆事件';
+    this.visible = true;
+    if (event && event.main) {
+      this.event_form.setValue({
+        main: event.extendedProps.main.split(',')?.map((name: string) => ({name})),
+        member: event.extendedProps.member.split(',')?.map((name: string) => ({name})),
+      });
+    }
+    if (event) {
+      this.event_form.patchValue({
+        subject: event.title,
+        main: event.extendedProps.main,
+        member: event.extendedProps.member,
+        allday: event.allDay,
+        start_date: event.start,
+        end_date: event.end,
+        type: event.extendedProps.type,
+      });
+      this.showedit = true;
+    } else {
+      this.event_form.reset();
+      this.showedit = false;
+    }
   }
-  if (event) {
-    this.event_form.patchValue({
-      subject: event.title,
-      main: event.extendedProps.main,
-      member: event.extendedProps.member,
-      allday: event.allDay,
-      start_date: event.start,
-      end_date: event.end,
-      type: event.extendedProps.type,
-    });
-    this.showedit = true;
-  } else {
-    this.event_form.reset();
-    this.showedit = false;
-  }
-}
 
 //類型選擇變化
-typeValue(event: any): void {
-  const typeValue = this.type.find((s: { code: any; }) => s.code === event.value.code);
-  console.log(event.value.code, typeValue.name);
-}
+  typeValue(event: any): void {
+    const typeValue = this.type.find((s: { code: any; }) => s.code === event.value.code);
+    console.log(event.value.code, typeValue.name);
+  }
 
 }
 
