d36dcaa4346f04847290d7b2b62f563c6172a722
zowie <C109118211@nkust.edu.tw>
2023-08-07 14:50:43 +0800

    feat: test TreeTable

--- src/app/views/features/role/role/role.component.ts
+++ src/app/views/features/role/role/role.component.ts
@@ -21,6 +21,26 @@ export class RoleComponent {
       created_at: "2023-04-15",
     }
   ];
+  test: any[] =[
+    {
+      id: 1000,
+      name: 'James Butt',
+      country: {
+        name: 'Algeria',
+        code: 'dz'
+      },
+      company: 'Benton, John B Jr',
+      date: '2015-09-13',
+      status: 'unqualified',
+      verified: true,
+      activity: 17,
+      representative: {
+        name: 'Ioni Bowcher',
+        image: 'ionibowcher.png'
+      },
+      balance: 70663
+    },
+  ]
   roleForm: UntypedFormGroup;
   constructor(
     private fb: UntypedFormBuilder,
